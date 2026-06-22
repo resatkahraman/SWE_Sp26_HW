94b1b7793a754291f6cf174b82899ef15df269c0
Reşat Kahraman <resat.kahraman@outlook.com>
2026-06-22 16:59:11 +0300

    Add TLE datetime to view

--- app/main/views.py
+++ app/main/views.py
@@ -6,24 +6,25 @@ import requests
 
 @main.route('/',methods=['GET'])
 def index():
-	return render_template('index.html', title='Home')
+    return render_template('index.html', title='Home')
 
 @main.route('/calculate',methods=['GET','POST'])
 def calculate():
-	form = DateTimeForm()
-	satellite_id = "25544" # ISS Zarya
-	result = None
-	if form.validate_on_submit():
-		s_date_time = form.input_date_time.data
-		(query, headers) = build_query(satellite_id)
-		response = requests.get(query, headers=headers)
-		tle_dict = parse_response(response)
-		if tle_dict:
-			(error, r, v) = parse_satellite(tle_dict, s_date_time)
-			if error == 0:
-				result = {"tle_date_time":"TLE DateTime: ", "l1":f"Position (X, Y, Z) in km: {r}", "l2":f"Velocity (dx, dy, dz) in km/s: {v}", "msg":"Satellite/space object position and velocity calculated"}
-			else:
-				result = {"msg":"Something went wrong. Try again."}	
-		else:
-			result = {"msg":"Something went wrong. Try again."}
-	return render_template('calculate.html', title='Earth', form=form, result=result)
+    form = DateTimeForm()
+    satellite_id = "25544" # ISS Zarya
+    result = None
+    if form.validate_on_submit():
+       s_date_time = form.input_date_time.data
+       (query, headers) = build_query(satellite_id)
+       response = requests.get(query, headers=headers)
+       tle_dict = parse_response(response)
+       if tle_dict:
+          (error, r, v) = parse_satellite(tle_dict, s_date_time)
+          if error == 0:
+             # tle_date_time kısmına tle_dict['date'] verisini ekledik
+             result = {"tle_date_time":f"TLE DateTime: {tle_dict['date']}", "l1":f"Position (X, Y, Z) in km: {r}", "l2":f"Velocity (dx, dy, dz) in km/s: {v}", "msg":"Satellite/space object position and velocity calculated"}
+          else:
+             result = {"msg":"Something went wrong. Try again."}
+       else:
+          result = {"msg":"Something went wrong. Try again."}
+    return render_template('calculate.html', title='Earth', form=form, result=result)
